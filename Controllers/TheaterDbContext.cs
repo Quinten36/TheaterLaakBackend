45c2429b688823757ed2f9e455f011ffc75f47ac
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2022-12-21 14:48:36 +0100

    edit program.cs to add sqlite builder

--- Controllers/TheaterDbContext.cs
+++ Controllers/TheaterDbContext.cs
@@ -1,12 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using TheaterLaakBackend.Models;
 
 namespace TheaterLaakBackend.Controllers;
 
 public class TheaterDbContext : DbContext
 {
+    public TheaterDbContext(DbContextOptions<TheaterDbContext> options) : base(options)
+    {
+        
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=database.db");
     }
+
+    public DbSet<Account> Account { get; set; }
+    public DbSet<AccountTagPivot> AccountTagPivot { get; set; }
+    public DbSet<Artist> Artist { get; set; }
+
 }
--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ using TheaterLaakBackend.Controllers;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TheaterDbContext>(options =>
+    options.UseSqlite("Data Source=Database.db"));builder.Services.AddControllers();
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
