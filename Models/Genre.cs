cec9f6d7a11bb15504ea7fa7ad41ffc789d9c634
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2022-12-23 12:57:32 +0100

    make genre contain relations

---
+++ Models/Genre.cs
@@ -0,0 +1,11 @@
+namespace TheaterLaakBackend.Models;
+
+public class Genre
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public List<Account> Accounts { get; } = new();
+    public List<Program> Programs { get; } = new();
+    
+}
