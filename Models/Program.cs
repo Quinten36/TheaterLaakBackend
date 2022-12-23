7a746c05c3c9f9af077f7613721dd7b4532d953d
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2022-12-23 12:58:21 +0100

    make the program model

--- Models/Program.cs
+++ Models/Program.cs
@@ -2,10 +2,22 @@ namespace TheaterLaakBackend.Models;
 
 public class Program
 {
-    public int id { get; set; }
-    public string title { get; set; }
-    public string description { get; set; }
-    public bool isExclusive { get; set; }
-    public DateTime beginExclusiveSale { get; set; }
-    public DateTime beginSale { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsExclusive { get; set; }
+    public string Image { get; set; }
+    public DateTime BeginExclusiveSale { get; set; }
+    public DateTime BeginSale { get; set; }
+
+    public List<Genre> Genres { get; } = new();
+
+    public int GroupId { get; set; }
+    public Group Group { get; set; }
+
+    public List<Show> Shows { get; } = new();
 }
+
+//TODO: price uit SQL model weghalen
+//TODO: Image toevoegen in sql model
+//TODO: Andere Naam?
