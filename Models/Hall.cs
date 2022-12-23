d1595154e5769c7a05806c1a94667b7333ea69b7
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2022-12-23 12:57:56 +0100

    make hall contain relations

--- Models/Hall.cs
+++ Models/Hall.cs
@@ -2,6 +2,15 @@ namespace TheaterLaakBackend.Models;
 
 public class Hall
 {
-    public int id { get; set; }
-    public int capacity { get; set; }
+    public int Id { get; set; }
+    public int Capacity { get; set; }
+
+    public List<Show> Shows { get; } = new();
+    public List<Seat> Seats { get; } = new();
+    public List<Reservation> Reservations { get; } = new();
 }
+
+//TODO: Toevoegen FirstClassSeats in sql diagram
+//TODO: Toevoegen SecondClassSeats in sql diagram
+//TODO: Toevoegen ThirdClassSeats in sql diagram
+//TODO: aanpassen capacity in sql diagram moet null kunnen zijn
