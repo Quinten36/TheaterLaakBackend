a9c37f2f1e35a1af000cbdb8e6524e19ed49a534
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2022-12-23 13:00:50 +0100

    make ticket model contain relations

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -2,9 +2,17 @@ namespace TheaterLaakBackend.Models;
 
 public class Ticket
 {
-    public int id { get; set; }
-    public int seat { get; set; }
-    public bool hasPaid { get; set; }
-    public int account { get; set; }
-    public int show { get; set; }
+    public int Id { get; set; }
+    
+    public int SeatId { get; set; }
+    public Seat Seat { get; set; }
+    
+    public int AccountId { get; set; }
+    public Account Account { get; set; }
+    
+    public int ShowId { get; set; }
+    public Show Show { get; set; }
+
+
+
 }
