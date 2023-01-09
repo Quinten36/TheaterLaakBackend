f03382100240a33196a691423a2e0852b42775e2
QuintenK <codestuff4all@gmail.com>
2023-01-09 16:42:52 +0100

    added a limitter endpoint to only get a few results

--- Controllers/ShowController.cs
+++ Controllers/ShowController.cs
@@ -9,115 +9,131 @@ using TheaterLaakBackend.Models;
 
 namespace TheaterLaakBackend.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ShowController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ShowController : ControllerBase
+  {
+    private readonly TheaterDbContext _context;
+
+    public ShowController(TheaterDbContext context)
     {
-        private readonly TheaterDbContext _context;
-
-        public ShowController(TheaterDbContext context)
-        {
-            _context = context;
-        }
-
-        // GET: api/Show
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Show>>> GetShows()
-        {
-          if (_context.Shows == null)
-          {
-              return NotFound();
-          }
-            return await _context.Shows.ToListAsync();
-        }
-
-        // GET: api/Show/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Show>> GetShow(int id)
-        {
-          if (_context.Shows == null)
-          {
-              return NotFound();
-          }
-            var show = await _context.Shows.FindAsync(id);
-
-            if (show == null)
-            {
-                return NotFound();
-            }
-
-            return show;
-        }
-
-        // PUT: api/Show/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutShow(int id, Show show)
-        {
-            if (id != show.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(show).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ShowExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Show
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Show>> PostShow(Show show)
-        {
-          if (_context.Shows == null)
-          {
-              return Problem("Entity set 'TheaterDbContext.Shows'  is null.");
-          }
-            _context.Shows.Add(show);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetShow", new { id = show.Id }, show);
-        }
-
-        // DELETE: api/Show/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteShow(int id)
-        {
-            if (_context.Shows == null)
-            {
-                return NotFound();
-            }
-            var show = await _context.Shows.FindAsync(id);
-            if (show == null)
-            {
-                return NotFound();
-            }
-
-            _context.Shows.Remove(show);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ShowExists(int id)
-        {
-            return (_context.Shows?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+        _context = context;
     }
+
+    // GET: api/Show
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Show>>> GetShows()
+    {
+      if (_context.Shows == null)
+      {
+        return NotFound();
+      }
+        return await _context.Shows.ToListAsync();
+    }
+
+    // GET: api/Show/limit/5
+    [HttpGet("limit/{id}")]
+    public async Task<ActionResult<IEnumerable<Show>>> GetLimitedShows()
+    {
+      if (_context.Shows == null)
+      {
+        return NotFound();
+      }
+
+      List<Show> shows = await _context.Shows.ToListAsync();
+      //TODO: alleen shows selecteren die na de dag van vandaag plaats vinden. of die populair zijn
+      List<Show> limitedShow = shows.OrderBy(i => i.Start).Take(5).ToList();
+
+      return limitedShow;
+    }
+
+    // GET: api/Show/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Show>> GetShow(int id)
+    {
+      if (_context.Shows == null)
+      {
+        return NotFound();
+      }
+      var show = await _context.Shows.FindAsync(id);
+
+      if (show == null)
+      {
+        return NotFound();
+      }
+
+      return show;
+    }
+
+    // PUT: api/Show/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutShow(int id, Show show)
+    {
+      if (id != show.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(show).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ShowExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Show
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Show>> PostShow(Show show)
+    {
+      if (_context.Shows == null)
+      {
+        return Problem("Entity set 'TheaterDbContext.Shows'  is null.");
+      }
+      _context.Shows.Add(show);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetShow", new { id = show.Id }, show);
+    }
+
+    // DELETE: api/Show/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteShow(int id)
+    {
+      if (_context.Shows == null)
+      {
+        return NotFound();
+      }
+      var show = await _context.Shows.FindAsync(id);
+      if (show == null)
+      {
+        return NotFound();
+      }
+
+      _context.Shows.Remove(show);
+      await _context.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool ShowExists(int id)
+    {
+      return (_context.Shows?.Any(e => e.Id == id)).GetValueOrDefault();
+    }
+  }
 }
