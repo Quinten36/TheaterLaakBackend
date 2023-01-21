798c508188e67e5a10ad028a5b06833ded939b16
Dtvanaanhout <94524307+Dtvanaanhout@users.noreply.github.com>
2023-01-21 12:48:35 +0100

    made nullable

--- Models/Show.cs
+++ Models/Show.cs
@@ -10,13 +10,13 @@ public class Show
     public double? ThirdClassPrice { get; set; }
     
     public int HallId { get; set; }
-    public Hall Hall { get; set; }
+    public Hall? Hall { get; set; }
     
     public int ProgramId { get; set; }
-    public Program Program { get; set; }
+    public Program? Program { get; set; }
     
     public int GroupId { get; set; }
-    public Group Group { get; set; }
+    public Group? Group { get; set; }
 
     public List<Seat> Seats { get; set; } = new();
     public List<Ticket> Tickets { get; set; } = new();
